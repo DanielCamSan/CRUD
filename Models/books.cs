f04fc8df7ddb5526f8f1c21831a3ddb573ba6ca1
unknown <ricardo.andrade@ucb.edu.bo>
2025-09-01 17:31:58 -0400

    creating books model

---
+++ Models/books.cs
@@ -0,0 +1,7 @@
+﻿   public class books
+    {
+    public Guid Id { get; set; }
+    public string Name { get; set; } = "";
+    public string Genre { get; set; } = "";
+    public int  Edition { get; set; }
+    }
