5fdc527e13b073d8c2379b3cb3199342fb1fb68e
Rodrigo40R <rovillaaya10@gmail.com>
2025-09-01 17:08:27 -0400

    feature: model of books

---
+++ Models/Books.cs
@@ -0,0 +1,13 @@
+﻿ public class Books
+ {
+    public Guid Id { get; set; }
+    public string autor { get; set; } = "";
+    public int year { get; set; }
+    public string title { get; set; } = "";
+
+
+
+
+
+}
+
