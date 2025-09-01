e2f98e8423bb329d3207c0a5ddde751278697f19
Majo <maria.sandoval.o@ucb.edu.bo>
2025-09-01 17:35:19 -0400

    users_controller_model

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace newCRUD.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private static readonly List<User> users = new()
+            {
+                new User { Id = Guid.NewGuid(), Name = "Maria", Email = "maria@gmail.com" },
+                new User { Id = Guid.NewGuid(), Name = "Amira", Email = "amira@gmail.com" }
+            };
+
+        // GET api/users
+        [HttpGet]
+        public ActionResult<IEnumerable<User>> GetAll()
+        {
+            return Ok(users);
+        }
+
+        // GET api/users/{id}
+        [HttpGet("{id:guid}")]
+        public ActionResult<User> GetOne(Guid id)
+        {
+            var user = users.FirstOrDefault(a => a.Id == id);
+            return user is null ? NotFound() : Ok(user);
+        }
+
+        // POST api/users
+        [HttpPost]
+        public ActionResult<User> Create([FromBody] User user)
+        {
+            user.Id = Guid.NewGuid();
+            users.Add(user);
+            return CreatedAtAction(nameof(GetOne), new { id = user.Id }, user);
+        }
+
+        // PUT api/users/{id}
+        [HttpPut("{id:guid}")]
+        public ActionResult<User> Update(Guid id, [FromBody] User user)
+        {
+            var index = users.FindIndex(a => a.Id == id);
+            if (index == -1) return NotFound();
+
+            user.Id = id; // conservar el mismo Id
+            users[index] = user;
+            return Ok(user);
+        }
+
+        // PATCH api/users/{id}
+        [HttpPatch("{id:guid}")]
+        public ActionResult<User> Patch(Guid id, [FromBody] User partial)
+        {
+            var user = users.FirstOrDefault(a => a.Id == id);
+            if (user is null) return NotFound();
+
+            // solo cambia si trae valor
+            if (!string.IsNullOrEmpty(partial.Name)) user.Name = partial.Name;
+            if (!string.IsNullOrEmpty(partial.Email)) user.Email = partial.Email;
+            return Ok(user);
+        }
+
+        // DELETE api/users/{id}
+        [HttpDelete("{id:guid}")]
+        public IActionResult Delete(Guid id)
+        {
+            var removed = users.RemoveAll(a => a.Id == id);
+            return removed == 0 ? NotFound() : NoContent();
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,7 @@
+﻿public class User
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = "";
+    public string Email { get; set; } = ""; 
+
+}
