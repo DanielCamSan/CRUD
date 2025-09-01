880a821eeb63856f78712b3f86d32abd1e8551bf
unknown <leonardo.camacho@ucb.edu.bo>
2025-09-01 17:13:36 -0400

    feat: creation team branch

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ builder.Services.AddControllers();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline q .
 
 app.UseHttpsRedirection();
 
