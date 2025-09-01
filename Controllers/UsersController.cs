d2fb949d074fbe5094ada967bc74350c1e682834
unknown <nicole.herbas@ucb.edu.bo>
2025-09-01 17:28:16 -0400

    create list of users

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace newCRUD.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private static readonly List<User> _users = new()
+            {
+                new User { Id = Guid.NewGuid(), Name = "Carlos", Age = 52 },
+                new User { Id = Guid.NewGuid(), Name = "Angel", Age = 21 }
+            };
+    }
+}
