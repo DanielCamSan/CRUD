438c212230c6b264d8823b510ac8081f22c80091
unknown <amira.choque@ucb.edu.bo>
2025-09-05 16:57:30 -0400

    model of movie

---
+++ Models/Movie.cs
@@ -0,0 +1,7 @@
+public class Movie
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; } = "";
+    public string Genre { get; set; } = "";
+    public int Year { get; set; }
+}
