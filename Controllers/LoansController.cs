376a48c468ae182d790009de2625ff0ddda32fa6
Flora <flora.ortiz@ucb.edu.bo>
2025-09-04 20:46:46 -0400

    feat: create LoansController

---
+++ Controllers/LoansController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace newCRUD.Controllers
+{
+    [ApiController]
+    [Route("library/[controller]")]
+    public class LoansController : ControllerBase
+    {
+    }
+}
