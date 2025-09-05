4d2de144c02717798d5618a1263edf0c2ef5db0a
unknown <ricardo.andrade@ucb.edu.bo>
2025-09-05 17:17:35 -0400

    feat: create user model correct

--- Models/user.cs
+++ Models/user.cs
@@ -1,5 +1,38 @@
-﻿\
-	public Class1()
-	{
-	}
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+public class User
+{
+    public Guid Id { get; set; }
+
+    [Required, StringLength(100)]
+    public string Name { get; set; } = string.Empty;
+
+    [Required, EmailAddress]
+    public string Email { get; set; } = string.Empty;
+
+    [Required, StringLength(50)]
+    public string Passsword { get; set; } = string.Empty;
+
+    [Range(0, 100)]
+    public int Age { get; set; }
+}
+
+// DTOs (entrada/salida para la API)
+public record CreateAnimalDto
+{
+    [Required, StringLength(100)]
+    public string Name { get; init; } = string.Empty;
+
+    [Required, StringLength(50)]
+    public string Password { get; init; } = string.Empty;
+
+}
+
+public record UpdateAnimalDto
+{
+    [Required, StringLength(100)]
+    public string Name { get; init; } = string.Empty;
+
+    [Range(0, 100)]
+    public int Age { get; init; }
 }
