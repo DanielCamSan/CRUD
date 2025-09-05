488332f6f4067a9561d5724cbc71666f140f23d2
Majo <maria.sandoval.o@ucb.edu.bo>
2025-09-05 17:36:42 -0400

    feat(subscription):DTOcreation

---
+++ Models/Subscription.cs
@@ -0,0 +1,28 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+public class Subscription
+{
+    public Guid Id { get; set; }
+
+    [Required]
+    public DateTime Subscription_Date { get; set; }
+    [Required]
+    public DateTime Duration { get; set; }
+    [StringLength(20), Required]
+    public string Name { get; set; } = string.Empty;
+    
+}
+
+// DTOs (entrada/salida para la API)
+public record CreateSubscriptionDto
+{
+    [Required]
+    public DateTime Subscription_Date { get; init; }
+
+    [Required]
+    public DateTime Duration { get; init; }
+
+    [StringLength(20), Required]
+    public string Name { get; init; } = string.Empty;
+}
+
