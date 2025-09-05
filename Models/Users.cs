8c8ac69148cc30e3a46b3ba00dd8331254093256
Caleb <calebbaving12@gmail.com>
2025-09-05 16:59:26 -0400

    feat: the CreateUserDto has been created

---
+++ Models/Users.cs
@@ -0,0 +1,35 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+public class User
+{
+    public Guid Id { get; set; }
+
+    [Required, StringLength(100)]
+    public string Name { get; set; } = string.Empty;
+
+    [Required, EmailAddress]
+    public string email { get; set; } = string.Empty;
+
+    [Range(0, 122)]
+    public int Age { get; set; }
+
+    [Required, StringLength(128)]
+    public string password { get; set; } = "";
+
+
+}
+public record CreateUserDto
+{
+    [Required, StringLength(100)]
+    public string Name { get; init; } = string.Empty;
+
+    [Range(0, 100)]
+    public int Age { get; init; }
+
+    [Required, EmailAddress]
+    public string email { get; init; } = string.Empty;
+
+    [Required, StringLength(128)]
+    public string password { get; init; } = "";
+}
