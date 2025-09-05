45cc117b16569b9c8e126face3364c58dd3ca2aa
Flora <flora.ortiz>
2025-09-05 17:09:31 -0400

    feat: create subscription model

---
+++ Models/Subscription.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+public class Subscription
+ {
+    public Guid Id { get; set; }
+
+    [Required]
+    public DateOnly SubscriptionDate { get; set; }
+
+    [Required]
+    public int duration { get; set; }
+
+    [Required, StringLength(100)]
+    public string name { get; set; } = string.Empty;
+}
