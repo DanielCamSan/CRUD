3f907058e6812b7d76865ad6b8039c0cf0dfae4b
Soria <rene.soria@ucb.edu.bo>
2025-09-05 17:22:13 -0400

    feat:add GET endpoints to MovieController

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace newCRUD.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MoviesController : ControllerBase
+    {
+        private static readonly List<Movie> _movies = new()
+        {
+            new Movie { Id = Guid.NewGuid(), Title = "Inception",   Genre = "Sci-Fi", Year = 2010 },
+            new Movie { Id = Guid.NewGuid(), Title = "The Godfather", Genre = "Crime",  Year = 1972 }
+
+        };
+
+        // READ: GET api/movies
+        [HttpGet]
+        public ActionResult<IEnumerable<Movie>> GetAll()
+            => Ok(_movies);
+
+        // READ: GET api/movies/{id}
+        [HttpGet("{id:guid}")]
+        public ActionResult<Movie> GetOne(Guid id)
+        {
+            var movie = _movies.FirstOrDefault(a => a.Id == id);
+            return movie is null
+                ? NotFound(new { error = "Movie not found", status = 404 })
+                : Ok(movie);
+        }
+    }
+}
