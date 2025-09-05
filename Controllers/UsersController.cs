d296ff132b2a2be009f7b60540c6e10c84e2d051
shalure10lure <yessicarado@gmail.com>
2025-09-05 17:03:37 -0400

    feat:adding userscontroller

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -1,6 +1,16 @@
-﻿namespace newCRUD.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace newCRUD.Controllers
 {
-    public class UsersController
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController:ControllerBase
     {
+        private static readonly List<Users> _users = new()
+        {
+            new Users { Id = Guid.NewGuid(), Name = "Carlos", Age = 25, email = "carlitos@gmail.com",password="123carlos"},
+            new Users { Id = Guid.NewGuid(), Name = "Mateo", Age = 27, email = "mat@gamil.com",password="mati345" }
+        };
+
     }
 }
