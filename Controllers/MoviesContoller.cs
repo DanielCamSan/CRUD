8e9081f2de30fa6ec9c9fc7466f9a6805d03cbfd
Fabio Garcia Meza <fabio.garciameza@ucb.edu.bo>
2025-09-05 17:21:26 -0400

    feat: HttpGet all and guid

--- Controllers/MoviesContoller.cs
+++ Controllers/MoviesContoller.cs
@@ -1,6 +1,27 @@
-﻿namespace newCRUD.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using newCRUD.Models;
+
+namespace newCRUD.Controllers
 {
-    public class MoviesContoller
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MoviesController : ControllerBase
     {
+        private static readonly List<Movie> _movies = new()
+        {
+            new Movie { Id = Guid.NewGuid(), Title = "The Matrix", Genre = "Sci-Fi", Year = 1999 },
+            new Movie { Id = Guid.NewGuid(), Title = "Spirited Away", Genre = "Animation", Year = 2001 }
+        };
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Movie>> GetAll()
+            => Ok(_movies);
+
+        [HttpGet("{id:guid}")]
+        public ActionResult<Movie> GetOne(Guid id)
+        {
+            var movie = _movies.FirstOrDefault(m => m.Id == id);
+            return movie is null ? NotFound(new { error = "Movie not found", status = 404 }) : Ok(movie);
+        }
     }
 }
