361160074bfd6a71985fe9af97e707da719b75d1
Majo <maria.sandoval.o@ucb.edu.bo>
2025-09-05 17:42:02 -0400

    feat(subscription):DTOupdate

--- Models/Subscription.cs
+++ Models/Subscription.cs
@@ -26,3 +26,14 @@ public record CreateSubscriptionDto
     public string Name { get; init; } = string.Empty;
 }
 
+public record UpdateAnimalDto
+{
+    [Required]
+    public DateTime Subscription_Date { get; init; } 
+
+    [Required]
+    public DateTime Duration { get; init; }
+
+    [StringLength(20), Required]
+    public string Name { get; init; } = string.Empty;
+}
