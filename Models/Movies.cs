13725de42ca537e9fda269fb4f14e0d433d97a9a
oriana <oriana.flores@ucb.edu.bo>
2025-09-05 17:26:42 -0400

    movies

---
+++ Models/Movies.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+public class Movie
+{
+    public Guid Id { get; set; }
+
+    [Required, StringLength(100)]
+    public string Title { get; set; } = string.Empty;
+
+    [Range(1900, 2100)]
+    public int Year { get; set; }
+
+    [Required, StringLength(50)]
+    public string Genre { get; set; } = string.Empty;
+}
+
+// DTOs
+public record CreateMovieDto
+{
+    [Required, StringLength(100)]
+    public string Title { get; init; } = string.Empty;
+
+    [Range(1900, 2100)]
+    public int Year { get; init; }
+
+    [Required, StringLength(50)]
+    public string Genre { get; init; } = string.Empty;
+}
+
+public record UpdateMovieDto
+{
+    [Required, StringLength(100)]
+    public string Title { get; init; } = string.Empty;
+
+    [Range(1900, 2100)]
+    public int Year { get; init; }
+
+    [Required, StringLength(50)]
+    public string Genre { get; init; } = string.Empty;
+}
+
+
+
