400bcce64781bec7c5b181dea386d2353625a94e
Flora <flora.ortiz>
2025-09-05 17:32:51 -0400

    feat: creating SubscriptionsContoller & mock list

---
+++ Controllers/SubscriptionsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace newCRUD.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SubscriptionsController : ControllerBase
+    {
+        private static readonly List<Subscription> subscriptions = new()
+        {
+            new Subscription {Id = Guid.NewGuid(), Name = "Andy", Duration = 365, SubscriptionDate = DateOnly.FromDateTime(DateTime.Now) },
+            new Subscription {Id = Guid.NewGuid(), Name = "Dabner", Duration = 365, SubscriptionDate = DateOnly.FromDateTime(DateTime.Now) }
+        };
+
+
+    }
+}
