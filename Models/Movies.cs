aaf15df634fbb3297cea172f5c2b0922d53e7f24
Fabio Garcia Meza <fabio.garciameza@ucb.edu.bo>
2025-09-05 17:07:44 -0400

    feat: adding movies.cs

--- Models/Movies.cs
+++ Models/Movies.cs
@@ -1,6 +1,19 @@
-﻿namespace newCRUD.Models
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace newCRUD.Models
 {
-    public class Movies
+    public class Movie
     {
+        public Guid Id { get; set; }
+
+        [Required, StringLength(150)]
+        public string Title { get; set; } = string.Empty;
+
+        [Required, StringLength(60)]
+        public string Genre { get; set; } = string.Empty;
+
+        [Range(1888, 2100)]
+        public int Year { get; set; }
     }
 }
