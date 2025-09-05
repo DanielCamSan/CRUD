737d7edbc61c47c8024b2ca8b1566c2851431b04
unknown <ricardo.andrade@ucb.edu.bo>
2025-09-05 17:03:52 -0400

    fead: create user model

--- Models/Animal.cs
+++ Models/Animal.cs
@@ -1,7 +1,41 @@
-﻿public class Animal
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+public class User
 {
     public Guid Id { get; set; }
-    public string Name { get; set; } = "";
-    public string Species { get; set; } = "";
+
+    [Required, StringLength(50)]
+    public string Name { get; set; } = string.Empty;
+
+    [Required, EmailAddress]
+    public string Email { get; set; } = string.Empty;
+
+    [Required, StringLength(12)]
+    public string Password { get; set; } = string.Empty;
+
+    [Range(0, 110)]
     public int Age { get; set; }
+}
+
+// DTOs 
+public record CreateUserDto
+{
+    [Required, StringLength(50)]
+    public string Name { get; init; } = string.Empty;
+
+    [Required, EmailAddress]
+    public string Gmail { get; set; } = string.Empty;
+
+}
+
+public record UpdateAnimalDto
+{
+    [Required, StringLength(50)]
+    public string Name { get; init; } = string.Empty;
+
+    [Required, EmailAddress]
+    public string Gmail { get; set; } = string.Empty;
+
+    [Range(0, 110)]
+    public int Age { get; init; }
 }
