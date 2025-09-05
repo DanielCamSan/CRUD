af63cc8a0874003095c776200403b07e8c02c396
Dabner <dabnerorozco123@gmail.com>
2025-09-04 21:41:42 -0400

    feat: creating UserController

---
+++ Controllers/UserController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace newCRUD.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : Controller
+    {
+        private static readonly List<User> users = new()
+            {
+                new User { Id = Guid.NewGuid(), Username = "dahbner", Age = 19 },
+                new User { Id = Guid.NewGuid(), Username = "andy.andrade", Age = 20 }
+            };
+    }
+}
