714744982a8c8b91135390b44bde1d3f40947b86
Maria <mariajesuslozada10@gmail.com>
2025-09-05 17:19:46 -0400

    feat: adding moviescontoller

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,23 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace newCRUD.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class MoviesController: ControllerBase
+    {
+        private static readonly List<Movies> _movies = new()
+        {
+            new Movies { Title = "Conjuro4", Gender = "Horror", Year = 2025 },
+            new Movies { Title = "Bestia", Gender = "Romantic", Year = 2020 }
+        };
+
+
+
+
+    }
+}
+
+       
