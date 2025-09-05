dd91788cacae3326ee118d498d054e349ecb5450
unknown <ricardo.andrade@ucb.edu.bo>
2025-09-04 22:19:27 -0400

    feed: creating books controller

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace newCRUD.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BooksController : Controller
+    {
+        private static readonly List<User> books = new()
+            {
+                new Book { Id = Guid.NewGuid(), name = "pride and prejudice ", genre = "novel",edition=1 },
+                new Book { Id = Guid.NewGuid(), name = "hunger games", genre = "drama",edition=2 }
+            };
+    }
+}
