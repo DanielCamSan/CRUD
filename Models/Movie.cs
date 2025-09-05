c62e369411e695035e26d25dc3597886c73dfab0
unknown <nicole.herbas@ucb.edu.bo>
2025-09-05 17:02:09 -0400

    model movie with updateDTO

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -25,4 +25,14 @@ public record CreateMovieDto
     [Range(0, 400)]
     public int Duration { get; set; }
 }
+public record UpdateMovieDto
+{
+    [Required, StringLength(50)]
+    public string Name { get; set; } = string.Empty;
 
+    [Required, StringLength(50)]
+    public string Category { get; set; } = string.Empty;
+
+    [Range(0, 400)]
+    public int Duration { get; set; }
+}
