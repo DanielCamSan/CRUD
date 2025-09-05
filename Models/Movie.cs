a23f66456280258f2d128f6005d17eb640c04889
unknown <nicole.herbas@ucb.edu.bo>
2025-09-05 16:55:59 -0400

    create Movie basic model

---
+++ Models/Movie.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+public class Movie
+{
+    public Guid Id { get; set; }
+
+    [Required, StringLength(50)]
+    public string Name { get; set; } = string.Empty;
+
+    [Required, StringLength(50)]
+    public string Category { get; set; } = string.Empty;
+
+    [Range(0, 400)]
+    public int Duration { get; set; }
+}
