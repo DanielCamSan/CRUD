eca282831a1f69377d9d08c46cdb09fd70d3a03e
Soria <rene.soria@ucb.edu.bo>
2025-09-05 17:00:23 -0400

    feat: adding movie model

---
+++ Models/Movie.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+public class Movie
+{
+    public Guid Id { get; set; }
+
+    [Required, StringLength(200)]
+    public string Title{ get; set; } = string.Empty;
+
+    [Required, StringLength(100)]
+    public string Genre { get; set; } = string.Empty;
+
+    [Range(1900, 2030)]
+    public int Year { get; set; }
+}
