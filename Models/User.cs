811c829bfc4e3b00198c1a17b4999f52a238da2c
Brenda-Medrano <brenda.medrano@ucb.edu.bo>
2025-09-05 17:55:45 -0400

    adding user controller

--- Models/User.cs
+++ Models/User.cs
@@ -1,10 +1,47 @@
-﻿namespace newCRUD.Models
+﻿
+using System;
+using System.ComponentModel.DataAnnotations;
+public class User
 {
-    public class User
-    {
-        public Guid Id { get; set; }
-        public string Name { get; set; } = "";
-        public string Email { get; set; } = "";
-        public string Phone { get; set; } = "";
-    }
+    public Guid Id { get; set; }
+
+    [Required, StringLength(100)]
+    public string Name { get; set; } = string.Empty;
+    [Range(0, 100)]
+    public int Age { get; set; }
+
+    [Required, StringLength(100)]
+    public string email { get; set; } = string.Empty;
+
+    [Required, StringLength(30)]
+    public string password { get; set; } = string.Empty;
+}
+
+// DTOs (entrada/salida para la API)
+public record CreateUserDto
+{
+    [Required, StringLength(100)]
+    public string Name { get; init; } = string.Empty;
+    [Range(0, 100)]
+    public int Age { get; init; }
+
+    [Required, StringLength(100)]
+    public string email { get; init; } = string.Empty;
+
+    [Required, StringLength(30)]
+    public string password { get; init; } = string.Empty;
+}
+
+public record UpdateUserDto
+{
+    [Required, StringLength(100)]
+    public string Name { get; init; } = string.Empty;
+    [Range(0, 100)]
+    public int Age { get; init; }
+
+    [Required, StringLength(100)]
+    public string email { get; init; } = string.Empty;
+
+    [Required, StringLength(30)]
+    public string password { get; init; } = string.Empty;
 }
