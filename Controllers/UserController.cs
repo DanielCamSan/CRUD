85a45ed49f8ec6786d65e69b725461df2533c5ce
Dabner <dabnerorozco123@gmail.com>
2025-09-04 21:47:01 -0400

    creating get by id - users

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -11,5 +11,19 @@ namespace newCRUD.Controllers
                 new User { Id = Guid.NewGuid(), Username = "dahbner", Age = 19 },
                 new User { Id = Guid.NewGuid(), Username = "andy.andrade", Age = 20 }
             };
+        // GET api/users
+        [HttpGet]
+        public ActionResult<IEnumerable<User>> GetAll()
+        {
+            return Ok(users);
+        }
+
+        // GET api/animals/{id}
+        [HttpGet("{id:guid}")]
+        public ActionResult<User> GetOne(Guid id)
+        {
+            var user = users.FirstOrDefault(a => a.Id == id);
+            return user is null ? NotFound() : Ok(user);
+        }
     }
 }
