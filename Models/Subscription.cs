de7f85f97c6b8b8aadb440115b71c85effc053e6
camilocro <camilorodriguezandre@gmail.com>
2025-09-05 17:05:21 -0400

    feat: Crear el modelo de datos para Subscription

---
+++ Models/Subscription.cs
@@ -0,0 +1,7 @@
+﻿public class Subscription
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = "";
+    public DateTime InDate { get; set; }
+    public int Duration { get; set; }
+}
