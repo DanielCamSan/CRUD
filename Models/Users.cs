3840cbd24ee7808199ee02dd338c0af80dc600b8
unknown <ernesto.rimassa@ucb.edu.bo>
2025-09-05 17:06:30 -0400

    Created Users in the Models Folder

---
+++ Models/Users.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+using System.ComponentModel.DataAnnotations;
+public class User
+{
+    public Guid Id { get; set; }
+
+    [Required, StringLength(100)]
+    public string Name { get; set; } = string.Empty;
+
+    [Required, StringLength(50)]
+    public EmailAddressAttribute Email { get; set; }
+
+    [Range(0, 100)]
+    public int Age { get; set; }
+}
