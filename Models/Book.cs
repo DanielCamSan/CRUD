8aee3c2150b2e3f3fe807e14108738b0a151f1e6
Brenda-Medrano <brenda.medrano@ucb.edu.bo>
2025-09-05 17:27:26 -0400

    adding users model

--- Models/Book.cs
+++
@@ -1,7 +0,0 @@
-﻿public class Book
-{
-    public Guid Id { get; set; }
-    public string Title { get; set; } = "";
-    public string Author { get; set; } = "";
-    public int Year { get; set; }
-}
