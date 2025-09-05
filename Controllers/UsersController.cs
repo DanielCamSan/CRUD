77fdfacd5060586f77e64fcbf9a094783f904fb6
shalure10lure <yessicarado@gmail.com>
2025-09-05 17:05:36 -0400

    feat:adding method get to userscontroller

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -11,6 +11,21 @@ namespace newCRUD.Controllers
             new Users { Id = Guid.NewGuid(), Name = "Carlos", Age = 25, email = "carlitos@gmail.com",password="123carlos"},
             new Users { Id = Guid.NewGuid(), Name = "Mateo", Age = 27, email = "mat@gamil.com",password="mati345" }
         };
+        // READ: GET api/users
+        [HttpGet]
+        public ActionResult<IEnumerable<Users>> GetAll()
+            => Ok(_users);
+
+        // READ: GET api/users/{id}
+        [HttpGet("{id:guid}")]
+        public ActionResult<Users> GetOne(Guid id)
+        {
+            var user = _users.FirstOrDefault(a => a.Id == id);
+            return user is null
+                ? NotFound(new { error = "User not found", status = 404 })
+                : Ok(user);
+        }
+
 
     }
 }
