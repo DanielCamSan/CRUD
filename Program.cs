26dac562736fcfbcff75afc351f788604d67bdcf
Flora <flora.ortiz@ucb.edu.bo>
2025-09-08 17:10:54 -0400

    feat: adding specific cors

--- Program.cs
+++ Program.cs
@@ -1,6 +1,21 @@
+using Microsoft.AspNetCore.RateLimiting;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("MyCorsPolicy", policy =>
+    {
+        policy.WithOrigins(
+            "http://127.0.0.1:5510")
+        .AllowAnyMethod()
+        .AllowAnyHeader();
+    }
+    );
+}
+);
 
 builder.Services.AddControllers();
 
@@ -9,6 +24,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 
 app.UseHttpsRedirection();
+
+app.UseCors("MyCorsPolicy");
 
 app.UseAuthorization();
 
