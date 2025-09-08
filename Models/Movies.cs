958e25c1f8cd5ab688f7f81dff42f2c17a48f525
Fabio Garcia Meza <fabio.garciameza@ucb.edu.bo>
2025-09-07 22:33:45 -0400

    Adding Movies.cs to Models

---
+++ Models/Movies.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace newCRUD.Models
+{
+    public class Movie
+    {
+        public Guid Id { get; set; }
+
+        [Required, StringLength(200)]
+        public string Title { get; set; } = string.Empty;
+
+        [Required, StringLength(100)]
+        public string Genre { get; set; } = string.Empty;
+
+        [Range(1888, 2100)] 
+        public int Year { get; set; }
+    }
+
+    public record CreateMovieDto
+    {
+        [Required, StringLength(200)]
+        public string Title { get; init; } = string.Empty;
+
+        [Required, StringLength(100)]
+        public string Genre { get; init; } = string.Empty;
+
+        [Range(1888, 2100)]
+        public int Year { get; init; }
+    }
+
+    public record UpdateMovieDto
+    {
+        [Required, StringLength(200)]
+        public string Title { get; init; } = string.Empty;
+
+        [Required, StringLength(100)]
+        public string Genre { get; init; } = string.Empty;
+
+        [Range(1888, 2100)]
+        public int Year { get; init; }
+    }
+}
