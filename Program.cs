86e5b6542b49a62d66939ded32938cab0df454dc
Luis Aguilar <luis.aguilar.c@ucb.edu.bo>
2025-09-08 17:19:54 -0400

    adding CORS and limit

--- Program.cs
+++ Program.cs
@@ -1,6 +1,32 @@
+using Microsoft.AspNetCore.RateLimiting;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("MiPoliticaCorsTeam03", policy =>
+    {
+        policy.WithOrigins("https://localhost:7162",
+            "http://127.0.0.1:5510")
+        .AllowAnyMethod()
+        .AllowAnyHeader();
+    });
+});
+
+builder.Services.AddRateLimiter(options =>
+{
+    options.RejectionStatusCode = StatusCodes.Status429TooManyRequests;
+    options.OnRejected = async (ctx, ct) =>
+    await ctx.HttpContext.Response.WriteAsync("Too many attempts, try it in one minute");
+    options.AddFixedWindowLimiter("default", config =>
+    {
+        config.PermitLimit = 4;
+        config.Window = TimeSpan.FromMinutes(1);
+        config.QueueLimit = 0;
+    });
+});
 
 builder.Services.AddControllers();
 
@@ -9,6 +35,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 
 app.UseHttpsRedirection();
+
+app.UseCors("MiPoliticaCorsTeam03");
 
 app.UseAuthorization();
 
