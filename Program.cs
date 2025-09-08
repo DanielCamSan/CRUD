f8501eb862990495f1abc68e0551f516e83d941b
Adrián Mercado <you@example.com>
2025-09-08 17:11:07 -0400

    Feat: Added cors

--- Program.cs
+++ Program.cs
@@ -1,6 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.WithOrigins("http://127.0.0.1:5500")
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
 
 builder.Services.AddControllers();
 
