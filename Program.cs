0681ca6227d09b64c86498af6961101637dd1994
Caleb <calebbaving12@gmail.com>
2025-09-08 17:06:20 -0400

    feat: the rate limiter and Cors have been created for the request pipeline in Program.cs

--- Program.cs
+++ Program.cs
@@ -1,5 +1,32 @@
+using Microsoft.AspNetCore.RateLimiting;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("MyPolicyCors", policy =>
+    {
+        policy.WithOrigins(
+            "http://127.0.0.1:5500")
+                .AllowAnyMethod()
+                .AllowAnyHeader();
+    });
+});
+
+builder.Services.AddRateLimiter(options =>
+{
+    options.RejectionStatusCode = StatusCodes.Status429TooManyRequests;
+    options.OnRejected = async (ctx, ct) =>
+    await ctx.HttpContext.Response.WriteAsync("Too many requests sent, try again in 5 minutes", ct);
+    options.AddFixedWindowLimiter("default", config =>
+    {
+        config.PermitLimit = 6;
+        config.Window = TimeSpan.FromMinutes(5);
+        config.QueueLimit = 6;
+
+    });
+
+});
 // Add services to the container.
 
 builder.Services.AddControllers();
@@ -11,7 +38,9 @@ var app = builder.Build();
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+app.UseRateLimiter();
+app.UseCors("MyPolicyCors");
 
-app.MapControllers();
+app.MapControllers().RequireRateLimiting("default");
 
 app.Run();
