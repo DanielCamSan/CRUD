ecd4816cbc6fb2ed0ae1c7d462a957cc2f9f61a2
Rodrigo40R <rovillaaya10@gmail.com>
2025-09-08 17:03:54 -0400

    F:Cors added and the rate-limiting

--- Program.cs
+++ Program.cs
@@ -1,4 +1,44 @@
+using Microsoft.AspNetCore.RateLimiting;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("MiPoliticaCors", policy =>
+    {
+        policy.WithOrigins("https://localhost:7162",
+                  "http://127.0.0.1:5500")
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
+// ==== Rate Limiting ====
+builder.Services.AddRateLimiter(options =>
+{
+    // Opcional: fija un código por defecto (igual lo podemos setear en OnRejected)
+    options.RejectionStatusCode = StatusCodes.Status429TooManyRequests;
+    options.OnRejected = async (ctx, ct) =>
+    await ctx.HttpContext.Response.WriteAsync("Too many attempts, try it in one minute", ct);
+
+
+    options.AddFixedWindowLimiter("default", config =>
+    {
+        config.PermitLimit = 3;                    //numero de requests
+        config.Window = TimeSpan.FromMinutes(1);     // en 1 minuto
+        config.QueueLimit = 0;                       // sin cola
+    });
+});
 
 // Add services to the container.
 
@@ -10,6 +50,27 @@ var app = builder.Build();
 
 app.UseHttpsRedirection();
 
+app.UseCors("MiPoliticaCors");
+
+
+app.UseAuthorization();
+app.UseRateLimiter();
+app.MapControllers().RequireRateLimiting("default");
+// Aplica la política a todos los controladores
+
+
+app.Run();
+
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseCors();
 app.UseAuthorization();
 
 app.MapControllers();
