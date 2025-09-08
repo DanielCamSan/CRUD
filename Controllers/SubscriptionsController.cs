50b2161b9efc9cef58f3338a39fff040545b19ac
Flora <flora.ortiz@ucb.edu.bo>
2025-09-07 23:33:55 -0400

    feat: GET subscriptions

--- Controllers/SubscriptionsController.cs
+++ Controllers/SubscriptionsController.cs
@@ -31,6 +31,31 @@ namespace newCRUD.Controllers
                 : src.OrderBy(x => prop.GetValue(x));
         }
 
+        [HttpGet]
+        public IActionResult GetAll(
+            [FromQuery] int? page,
+            [FromQuery] int? limit,
+            [FromQuery] string? sort,    
+            [FromQuery] string? order,   
+            [FromQuery] string? q      
+        )
+        {
+            var (p, l) = NormalizePage(page, limit);
+            IEnumerable<Subscription> query = subscriptions;
 
+            if (!string.IsNullOrWhiteSpace(q))
+                query = query.Where(s => s.Name.Contains(q, StringComparison.OrdinalIgnoreCase));
+
+            query = OrderByProp(query, sort, order);
+
+            var total = query.Count();
+            var data = query.Skip((p - 1) * l).Take(l).ToList();
+
+            return Ok(new
+            {
+                data,
+                meta = new { page = p, limit = l, total }
+            });
+        }
     }
 }
